649932bae988aa38608f776db7a709d2e0c91688
Beman <bdawes@acm.org>
2018-07-28 14:08:04 -0400

    Add initial infrastructure, test program, and header

---
+++ test/union_finder_test.cpp
@@ -0,0 +1,7 @@
+//  union_finder_test.cpp  -------------------------------------------------------------//
+
+//  © Copyright Beman Dawes, 2018
+
+//  Licensed under the Boost License Version 1.0; see www.boost.org/LICENSE_1_0.txt
+
+#include <union_finder.hpp>
